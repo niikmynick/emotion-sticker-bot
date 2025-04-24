9f7d8ba363ceee38f4e5aec30c021840ba18df04
Nikita <fk724s@mail.ru>
2025-04-24 21:50:54 +0300

    path fixes

--- utils/video.py
+++ utils/video.py
@@ -2,14 +2,13 @@ import os
 import cv2
 import logging
 
+from properties import VIDEO_DIR_PATH, FRAME_DIR_PATH
 
 logger = logging.getLogger(__name__)
-videos_path = './data/video/'
-frames_path = './data/frames/'
 
 
 def get_frames(filename: str) -> list:
-    full_path = os.path.join(videos_path, filename)
+    full_path = os.path.join(VIDEO_DIR_PATH, filename)
 
     if not os.path.exists(full_path):
         logger.error(f'File {full_path} does not exist')
@@ -18,18 +17,18 @@ def get_frames(filename: str) -> list:
         logger.error(f'{full_path} is not a file')
 
     logger.debug(f'Reading frames from {full_path}')
-    temp_frames_path = frames_path + f'video_{len(os.listdir(frames_path))}/'
+    temp_frames_path = FRAME_DIR_PATH + f'video_{len(os.listdir(FRAME_DIR_PATH))}/'
 
     capture = cv2.VideoCapture(full_path)
 
-    frameNr = 0
+    frame_number = 0
     frames = []
 
-    while (True):
+    while True:
         success, frame = capture.read()
 
         if success:
-            frame_path = temp_frames_path + f'frame_{frameNr}.jpg'
+            frame_path = temp_frames_path + f'frame_{frame_number}.jpg'
             frames.append(frame_path)
 
             cv2.imwrite(frame_path, frame)
@@ -37,7 +36,7 @@ def get_frames(filename: str) -> list:
         else:
             break
 
-        frameNr = frameNr + 1
+        frame_number += 1
 
     capture.release()
 
